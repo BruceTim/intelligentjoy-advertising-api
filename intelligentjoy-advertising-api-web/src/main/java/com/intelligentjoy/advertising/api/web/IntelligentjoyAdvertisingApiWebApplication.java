75f9eb03de485c8633e644b8ce2ba9847db4a3ca
BruceTim <694040432@qq.com>
2020-11-23 22:57:05 +0800

    提交初始版本

---
+++ intelligentjoy-advertising-api-web/src/main/java/com/intelligentjoy/advertising/api/web/IntelligentjoyAdvertisingApiWebApplication.java
@@ -0,0 +1,13 @@
+package com.intelligentjoy.advertising.api.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentjoyAdvertisingApiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentjoyAdvertisingApiWebApplication.class, args);
+	}
+
+}
